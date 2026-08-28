7c57dbedb0da8d23b5442877745e2036c2b09a35
Sergey <serg.hexe@gmail.com>
2026-08-28 22:39:28 +0300

    Restyle catalog refresh action

--- api/app.py
+++ api/app.py
@@ -23,7 +23,7 @@ from domains.workspace_entitlements import WorkspaceAccess, read_workspace_acces
 from domains.yandex_market_webhooks_api import mount_yandex_market_webhook_routes
 
 
-app = FastAPI(title="HomTech Seller API", version="0.0.77")
+app = FastAPI(title="HomTech Seller API", version="0.0.78")
 
 
 def cors_origins() -> list[str]:
